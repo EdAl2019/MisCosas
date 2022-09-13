cecbbfac917d7c1392182504e37b1ddca70e144b
EdAl2019 <edwalberto22@gmail.com>
2022-09-13 14:22:12 -0600

    FORMULARIO MENOR DE EDAD

---
+++ TRANSPORTE/Config/global.php
@@ -0,0 +1,17 @@
+<?php
+//Ip de la pc servidor de base de datos
+define("DB_HOST","10.10.7.20");
+
+//Nombre de la base de datos
+define("DB_NAME", "ihttT");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "ihtt");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", '1htt2022.');
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+
