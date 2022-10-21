81c38d84ca41296137c32c1fa6c695cb16b9ca7d
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2022-10-20 23:30:03 -0600

    Validaciones

--- Produccion/TRANSPORTE/index.php
+++ Produccion/TRANSPORTE/index.php
@@ -9,6 +9,28 @@
     
     }
     else {
+
+              date_default_timezone_set('America/Tegucigalpa');
+
+
+
+
+            $f_a = date('H:i:s', time());
+
+            $f_i = date('06:00:00', time());
+
+            $f_f = date('18:00:00', time());
+
+            echo $f_a;
+            $input;
+            if ($f_a<$f_f && $f_a>$f_i) {
+             $input="";
+                # code...
+            }else  {
+                # code...
+            $input="disabled";
+            }
+
        
 
         ?>
@@ -114,7 +136,7 @@
                             <br>
                             <div class="card-footer text-center">
                                 <button type="submit" name="entrar" id="entrar"
-                                    class="submit btn btn-primary"><h4>ENTRAR</h4></button>
+                                    class="submit btn btn-primary" <?php echo $input; ?>><h4>ENTRAR</h4></button>
                             </div>
                           
                            
