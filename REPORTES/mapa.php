cc126e4fa5bfee7617b2b0c190b6ddf88f2974b0
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2022-11-14 08:54:50 -0600

    Create mapa.php

---
+++ REPORTES/mapa.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    require("./head.php");
+    ?>
+   
+</head>
+<body id="contenedor">
+<?php
+    require("./menu.php");
+    
+    ?>
+    <br>
+  
+    <h1>PUNTOS DE CONTROL</h1>
+    <div class="container"></div>
+    <div class="row">
+    <div class="col-xs-12" >
+    <div class="card">
+    <div class="card-body">
+        <div class="row">
+        <div class="col-xs-12">
+        <iframe src="https://www.google.com/maps/d/embed?mid=1taZOuvoe3aj8_UBX20NNmqgipmh_lnQ&hl=es&ehbc=2E312F" width="100%" height="300%"></iframe>
+      </div>
+     
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+   
+
+    <?php
+    require("./pie.php");
+    
+    ?>
+   
+</body>
+</html>
